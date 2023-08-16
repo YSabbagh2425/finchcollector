df1607050d03093b6244f2b991418fb22e58003c
ysabbagh2425 <yousif.sabbagh@yahoo.com>
2023-08-16 16:27:46 +1000

    Finish part 5 and done

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
     path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('finches/<int:finch_id>/associate_toy/<int:toy_id>/', views.associate_toy, name='associate_toy'),
+    path('finches/<int:finch_id>/unassociate_toy/<int:toy_id>/', views.unassociate_toy, name='unassociate_toy'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
